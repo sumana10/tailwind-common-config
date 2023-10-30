c3d982b618228a3868a0fd279a2e1d3ada5c5fd8
sumana10 <sumanaonlyme@gmail.com>
2023-10-30 23:34:06 +0530

    first commit

--- packages/ui/card.tsx
+++ packages/ui/card.tsx
@@ -18,7 +18,7 @@ export function Card({
       rel="noopener noreferrer"
       target="_blank"
     >
-      <h2>
+      <h2 className="text-3xl">
         {title} <span>-&gt;</span>
       </h2>
       <p>{children}</p>
--- packages/ui/index.tsx
+++ packages/ui/index.tsx
@@ -1,2 +1,3 @@
 // component exports
 export { Card } from "./card";
+export {Test} from "./test";
